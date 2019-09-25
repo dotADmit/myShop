5a9356fb7a517574331d9375c9cff935e2bde7e6
Дмитрий Андреев <djandreev89@mail.ru>
2019-09-26 08:05:15 +1000

    Добавьте файлы проекта.

---
+++ myShop/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myShop
+{
+    class Program
+    {
+        enum Status
+        {
+            Bonus = 9,
+            Event = 12,
+            HighRating = 10
+        }
+        struct Product
+        {
+            public int Id;
+            public string Name;
+            public string Description;
+            public int Price;
+            public Status Status;
+        }
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
